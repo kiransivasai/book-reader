c61eb03a53dbd2cadd8cad01c270488f234f4f5c
kiransivasai <kiransiva2060@gmail.com>
2022-09-25 21:30:36 +0530

    Connected with API and added packages

--- src/components/book-read/BookRead.js
+++ src/components/book-read/BookRead.js
@@ -1,7 +1,22 @@
-import React from "react";
+import axios from "axios";
+import React, { useState } from "react";
 import "./BookRead.scss";
 
 const BookRead = () => {
+  const [bookInput, setBookInput] = useState("");
+  const [audioURI, setAudioURI] = useState("");
+  const readBook = async () => {
+    const response = await axios.post("http://localhost:5000/get_audio", {
+      text: bookInput,
+      headers: {
+        "Access-Control-Allow-Origin": "*",
+      },
+      responseType: "blob",
+    });
+    const audioUrl = window.URL.createObjectURL(new Blob([response.data]));
+    setAudioURI(audioUrl);
+    console.log(audioUrl);
+  };
   return (
     <div className="book-read">
       <textarea
@@ -9,8 +24,18 @@ const BookRead = () => {
         cols="10"
         placeholder="Enter the story to read"
         className="book-input"
+        value={bookInput}
+        onChange={(e) => setBookInput(e.target.value)}
       />
-      <button className="book-btn">Play</button>
+      <button className="book-btn" onClick={(e) => readBook(e)}>
+        Play
+      </button>
+      {!!audioURI && (
+        <audio controls src={audioURI}>
+          Your browser does not support the
+          <code>audio</code> element.
+        </audio>
+      )}
     </div>
   );
 };
